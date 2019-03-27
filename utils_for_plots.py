4d8fa18c61f30cc9636b6b24015da028123cdd32
awegsche <a.wegscheider7141@gmail.com>
2019-03-27 12:17:38 +0100

    add utils for plots

---
+++ utils_for_plots.py
@@ -0,0 +1,17 @@
+PlotColors = [
+    ['#5060F0', '#000090'],
+    ['#E05020', '#A00A00'],
+    ['#10A010', '#108010'],
+    ['#E09000', '#805010'],
+    ['#10B0E0', '#0050E0'],
+    ['#B010F0', '#5000E0'],
+    ['#A0F010', '#508010'],
+]
+
+PlotColorsPale = [
+    ["skyblue", "steelblue"],
+    ["salmon", "firebrick"],
+    ["lawngreen", "forestgreen"],
+    ["sandybrown", "sienna"],
+    ["lightsalmon", "tomato"]
+]
